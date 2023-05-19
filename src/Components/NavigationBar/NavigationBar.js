2ba49cb134833a89792426ed94eb9c8ded0bdbf1
José Sanmiguel <jose.sanmiguel.12@gmail.com>
2023-05-19 18:02:20 -0500

    enrutando componentes

---
+++ src/Components/NavigationBar/NavigationBar.js
@@ -0,0 +1,47 @@
+import React from 'react'
+import Logo from '../../assets/icons/logo.svg'
+
+
+const NavigationBar = () => {
+    const navList = [
+        {
+            item: "00",
+            name: "Home",
+            url: ""
+        },
+        {
+            item: "01",
+            name: "Destination",
+            url: ""
+        },
+        {
+            item: "02",
+            name: "Crew",
+            url: ""
+        },
+        {
+            item: "03",
+            name: "Technology",
+            url: ""
+        },
+    ];
+
+return (
+    <container>
+        <div>
+            <figure>
+                <img src={Logo} alt='logo' />
+            </figure>
+        </div>
+        <div>
+            {navList.map((item, index) => (
+                <a key={`${item.item} - ${item.name} - ${item.index}`} href={item.url}>
+                    <li>{item.item}</li>
+                    <li>{item.name}</li>
+                </a>
+            ))}
+        </div>
+        </container>
+    )}
+
+export default NavigationBar;
---
+++ src/Routes/AppRouter.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Layout from "../Components/layout/Layout";
+import Home from "../Components/home/Home";
+import Crew from "../Components/crew/Crew";
+import Destination from "../Components/destination/Destination";
+import Technology from "../Components/technology/Technology";
+
+
+const AppRouter = () => {
+
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path={"/"} element={<Layout />}>
+            <Route index element={<Home />} />
+                <Route path="crew" element={<Crew />} />
+                <Route path="destination" element={<Destination />} />
+                <Route path="technology" element={<Technology />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+};
+
+export default AppRouter;
