df149fab6a5c48ebfb4316e1a0aa8e1cc7e65d56
José Sanmiguel <jose.sanmiguel.12@gmail.com>
2023-05-20 00:06:37 -0500

    enlazando Scss

---
+++ src/Components/technology/Technology.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import background from "../../assets/technology/background-technology-desktop.jpg";
+// import launchPortrait from "../../assets/technology/image-launch-vehicle-portrait.jpg";
+// import spaceportPortrait from "../assets/technology/image-spaceport-portrait.jpg";
+// import spacePortrait from "../assets/technology/image-space-capsule-portrait.jpg";
+// import launchLand from "../assets/technology/image-launch-vehicle-landscape.jpg";
+// import spaceportLand from "../assets/technology/image-spaceport-landscape.jpg";
+// import spaceLand from "../assets/technology/image-space-capsule-landscape.jpg";
+import './technology.scss';
+
+const Technology = () => {
+
+
+return (
+    <div>
+        <figure className='background'>
+            <img src={background} alt='background' />
+        </figure>
+    </div>
+    )
+}
+
+export default Technology
