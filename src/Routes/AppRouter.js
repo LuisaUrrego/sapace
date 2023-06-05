b9ab0115065996e107ea1645e2445254bdbdee0c
José Sanmiguel <jose.sanmiguel.12@gmail.com>
2023-06-04 20:56:04 -0500

    cambios para technology

--- src/Routes/AppRouter.js
+++ src/Routes/AppRouter.js
@@ -2,7 +2,7 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Layout from "../Components/layout/Layout";
 import Home from "../Components/home/Home";
-import Crew from "../Components/crew/Crew";
+// import Crew from "../Components/crew/Crew";
 import Destination from "../Components/destination/Destination";
 import Technology from "../Components/technology/Technology";
 import CrewSafe from "../Components/crewSafe/CrewSafe"
